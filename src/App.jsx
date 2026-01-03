1427cb7249c15592b39b39acaebd7e44e131328b
AndreiToma123 <118735556+AndreiToma123@users.noreply.github.com>
2026-01-03 15:42:41 +0200

    storage
    useState storage, display list on screen, save list to localStorage, delete items. load items from localStorage after page refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const savedItems = localStorage.getItem("pantryItems");
+    if (savedItems) {
+      setItems(JSON.parse(savedItems));
+    }
+  }, []);
+
   function handleSubmit(e) {
       e.preventDefault();
 
@@ -9,9 +18,22 @@ function App() {
       const formData = new FormData(form);
 
       const formJson = Object.fromEntries(formData.entries());
+
+      const updatedItems = ([...items, formJson]);
+      setItems(updatedItems);
+      localStorage.setItem("pantryItems", JSON.stringify(updatedItems));
+      form.reset();
       console.log(formJson);
     }
-    
+
+    function handleDelete(indexToDelete) {
+      const updatedItems = items.filter((item, index) => index!== indexToDelete);
+      setItems(updatedItems);
+      localStorage.setItem("pantryItems", JSON.stringify(updatedItems));
+    }
+
+
+
   return (
     <main className="container">
       <h1>Digital Pantry</h1>
@@ -20,10 +42,20 @@ function App() {
         Item: <input name="myItem" />
       </label>
       <label>
-        Expiry date: <input name="expiryDate" />
+        Expiry date: <input name="expiryDate" type="date" />
       </label>
       <button type="submit">Submit</button>
       </form>
+
+      <h2>Pantry Items</h2>
+      <ul>
+        {items.map((item, index) => (
+        <li key={index}>
+          {item.myItem}: {item.expiryDate}
+          <button onClick={() => handleDelete(index)}>Delete</button>
+        </li>
+        ))}
+      </ul>
     </main>
   );
 }
