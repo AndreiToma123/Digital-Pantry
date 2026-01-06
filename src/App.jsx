41515bb006eea2e321d4fd29cfa4adf7b1ea73d5
Andrei Stefan Toma <tomaandreistefan@gmail.com>
2026-01-06 16:59:53 +0200

    Added a search bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
   const [items, setItems] = useState([]);
+  const [query, setQuery] = useState("");
+
 
   useEffect(() => {
     const savedItems = localStorage.getItem("pantryItems");
@@ -47,22 +49,29 @@ function App() {
     return new Date(a.expiryDate) - new Date(b.expiryDate);
   });
 
+   const filteredItems = sortedItems.filter(item => 
+    item.myItem.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <main className="container">
       <h1>Digital Pantry</h1>
+       <label>
+          Search: <input type="text" onChange={e => setQuery(e.target.value)} />
+        </label>
       <form method="post" onSubmit={handleSubmit}>
         <label>
-          Item: <input name="myItem" />
+          Item <input name="myItem" />
         </label>
         <label>
-          Expiry date: <input name="expiryDate" type="date"/>
+          Expiry date <input name="expiryDate" type="date" />
         </label>
         <button type="submit">Submit</button>
       </form>
 
       <h2>Pantry Items</h2>
       <ul>
-        {sortedItems.map((item, index) => {
+        {filteredItems.map((item, index) => {
           const daysLeft = calculateDaysLeft(item.expiryDate);
           return (
             <li key={index}>
